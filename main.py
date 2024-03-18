14e640bbd2e131a722ad7caf297033bc44ebfd7a
Md Arafat Hossain <arafat.hossain@welldev.io>
2024-03-18 16:54:14 +0600

    database connected

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="12345678",
+  database="Student"
+)
+
+print(mydb)
+
 
 app = FastAPI()
 
